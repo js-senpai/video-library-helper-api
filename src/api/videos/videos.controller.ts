0d2a9335f09fec30a8a4932a117876aa905edb20
js-senpai <andrewkarapenkov@gmail.com>
2023-12-13 23:06:42 +0200

    Added new routes

--- src/api/videos/videos.controller.ts
+++ src/api/videos/videos.controller.ts
@@ -1,5 +1,21 @@
-import { Controller, Get, Logger, Param } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  ForbiddenException,
+  Get,
+  HttpStatus,
+  Logger,
+  Param,
+  ParseFilePipeBuilder,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { VideosService } from './videos.service';
+import { join } from 'path';
+import fs from 'fs';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller('videos')
 export class VideosController {
@@ -34,6 +50,68 @@ export class VideosController {
       return await this.videoService.getRandom();
     } catch (e) {
       this.logger.error(VideosController, 'Error in "getRandom" method', e);
+      throw e;
+    }
+  }
+
+  @Delete('/:name')
+  async delete(@Param('name') name: string) {
+    try {
+      return await this.videoService.delete(name);
+    } catch (e) {
+      this.logger.error(VideosController, 'Error in "delete" method', e);
+      throw e;
+    }
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('avatar', {
+      limits: {
+        files: 1,
+      },
+      storage: diskStorage({
+        destination: async function (req, file, cb) {
+          try {
+            const destinationPath = join(
+              __dirname,
+              '..',
+              '..',
+              '..',
+              '..',
+              `videos`,
+            );
+            if (!fs.existsSync(destinationPath)) {
+              await fs.promises.mkdir(destinationPath, { recursive: true });
+            }
+            const checkFilePath = join(destinationPath, file.originalname);
+            if (checkFilePath) {
+              throw new ForbiddenException(`The file is already exist`);
+            }
+            cb(null, destinationPath);
+          } catch (e) {
+            console.error(e);
+          }
+        },
+      }),
+    }),
+  )
+  async create(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: /(mp4)$/,
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
+    try {
+      return await this.videoService.create(file);
+    } catch (e) {
+      this.logger.error(VideosController, 'Error in "create" method', e);
       throw e;
     }
   }
--- src/api/videos/videos.service.ts
+++ src/api/videos/videos.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from '../../common/services/prisma.service';
 import { video } from '@prisma/client';
+import { IResponseOk } from '../../common/interfaces/response.interface';
+import { join } from 'path';
+import fs from 'fs';
 
 @Injectable()
 export class VideosService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async getAll(): Promise<Pick<video, 'name' | 'original_name'>[]> {
-    return await this.prismaService.video.findMany({
+    return this.prismaService.video.findMany({
       select: {
         name: true,
         original_name: true,
@@ -16,6 +23,48 @@ export class VideosService {
         name: 'asc',
       },
     });
+  }
+
+  async create(file: Express.Multer.File): Promise<video> {
+    const checkFile = await this.prismaService.video.findFirst({
+      where: {
+        original_name: file.filename,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (checkFile) {
+      throw new ForbiddenException(`The file is already exist`);
+    }
+    return this.prismaService.video.create({
+      data: {
+        original_name: file.filename,
+        name: file.originalname,
+      },
+    });
+  }
+
+  async delete(name: string): Promise<IResponseOk> {
+    const getFile = await this.prismaService.video.findFirst({
+      where: {
+        original_name: name,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (!getFile) {
+      throw new NotFoundException(`The file with name "${name}" not found`);
+    }
+    const getPath = join(__dirname, '..', '..', '..', '..', 'videos', name);
+    const checkPath = fs.existsSync(getPath);
+    if (checkPath) {
+      await fs.promises.unlink(getPath);
+    }
+    return {
+      ok: 'The video has successfully deleted',
+    };
   }
 
   async get(name: string): Promise<Pick<video, 'name' | 'original_name'>> {
